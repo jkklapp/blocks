da8ea65376e8aead49682a0d3967489765ad7b44
Jaakko Lappalainen <jkk.lapp@gmail.com>
2018-10-21 19:48:16 +0100

    Easier way to store the tree

--- merkle.py
+++ merkle.py
@@ -4,48 +4,33 @@ from utils import calc_hash_for_two_hashes
 class Merkle(object):
     def __init__(self):
         self.data = []
-
-    def add_leaf(self, t):
-        if not self.data:
-            self.data.append([])
-        l = len(self.data)
-        if len(self.data[l-1]) < 2:
-            self.data[l-1].append(t)
-        else:
-            self.data.append([t])
-        r = self.compute_root()
-        if len(self.data) >= 2:
-            self.data.pop(0)
-        self.data.insert(0, r)
-
-    def get_root(self):
-        return self.data[0]
-
-    def compute_root(self):
-        h_list = []
-        data = self.data
-        if len(data) > 1:
-            data = data[1:]
-
-        for i in range(0, len(data)):
-            h_list += data[i]
-            if len(data[i]) == 1:
-                h_list += data[i]  # Insert twice if leaf is single
-
-        l = len(h_list)
-
-        while l > 1:
-            a = h_list[l-1]
-            b = h_list[l-2]
-            h_list = h_list[:-2]
-            h = calc_hash_for_two_hashes(a, b)
-            h_list.insert(0, h)
-            l = len(h_list)
-
-        return h_list[0]
+        self.root = None
 
     def __repr__(self):
-        return "Tree:Root:" + str(self.data[0]) + ':Leaves:' + str(self.data[1:])
+        return "Tree:Root:" + str(self.root) + ':Leaves:' + str(self.data)
 
     def __len__(self):
         return len(self.data)
+
+    def _compute_root(self):
+        data = self.data[:]
+        if len(data) % 2 == 0:
+            data.append(data[-1])  # Repeat node if length is an odd number
+
+        # Compute intermediate hashes
+        while len(data) > 1:
+            a = data[len(data)-1]
+            b = data[len(data)-2]
+            data = data[:-2]
+            h = calc_hash_for_two_hashes(a, b)
+            data.insert(0, h)
+
+        return data[0]
+
+    def add_leaf(self, t):
+        self.data.append(t)
+        self.root = self._compute_root()
+        print self
+
+    def get_root(self):
+        return self.root
--- merkle_test.py
+++ merkle_test.py
@@ -6,17 +6,18 @@ class TestMerkle(unittest.TestCase):
     def setUp(self):
         self.tree = Merkle()
 
-    def test_block_inits_correctly(self):
+    def test_merkle_tree_inits_correctly(self):
         tree = self.tree
-
+        self.assertEqual(len(tree), 0)
         tree.add_leaf("0")
+        self.assertEqual(len(tree), 1)
         tree.add_leaf("1")
         self.assertEqual(len(tree), 2)
         tree.add_leaf("2")
         self.assertEqual(len(tree), 3)
         r1 = tree.get_root()
         tree.add_leaf("3")
-        self.assertEqual(len(tree), 3)
+        self.assertEqual(len(tree), 4)
         r2 = tree.get_root()
         self.assertNotEqual(r1, r2)
         tree.add_leaf("4")
@@ -24,7 +25,7 @@ class TestMerkle(unittest.TestCase):
         tree.add_leaf("6")
         tree.add_leaf("7")
         tree.add_leaf("8")
-        self.assertEqual(len(tree), 6)
+        self.assertEqual(len(tree), 9)
 
 
 if __name__ == '__main__':
